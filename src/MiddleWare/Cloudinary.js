10a9109a1b5d946200259bbb16a190ef45df172e
MOHAMED ABDIHAKIN MUHUMED <48615938+Nucmaan@users.noreply.github.com>
2024-08-13 04:43:09 +0800

    adding cloudinary
    adding cloudinary

--- src/MiddleWare/Cloudinary.js
+++ src/MiddleWare/Cloudinary.js
@@ -1,7 +1,7 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-    cloud_name: process.env.cloud_name,
+    cloud_name: process.env.CLOUD_NAME, // Update to match environment variable name
     api_key: process.env.API_KEY,
     api_secret: process.env.API_SECRET
 });
